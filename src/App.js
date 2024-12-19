eb1fa4eb286c1de87db55b1cfcd78bd8f2918eb4
Harrsha Kolli <70423957+t1ppu@users.noreply.github.com>
2024-12-19 14:55:48 -0700

    more css, image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles/main.scss';
+import './styles/images.scss';
+import myImage from './images/1.gif';
 
 function App() {
   return (
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">Sample Header</a>
-      <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#aboutus">About Us</a></li>
-      </ul>
-    </nav>
+    <div className="App">
+      <nav className="navbar" style = {{backgroundImage: {myImage}}}>
+        <a href="/" className="navbar-brand">Sample Header</a>
+        <ul className="navbar-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#aboutus">About Us</a></li>
+        </ul>
+      </nav>
+      <br></br>
+      <div> 
+        <img src={myImage} style={{borderRadius: '8px' }}/>
+      </div>
+    </div>
   );
 }
 
